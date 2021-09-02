ec139b3cd9ad323e34bb1615991e881ccb444344
emilasbringer <60388760+emilasbringer@users.noreply.github.com>
2021-09-02 13:22:58 +0200

    ye

--- src/main.java
+++ src/main.java
@@ -1,7 +1,4 @@
-import com.sun.tools.jconsole.JConsoleContext;
-
 import javax.swing.*;
-import java.awt.desktop.SystemEventListener;
 
 public class main {
     public static Boolean trueorfalse = null;
@@ -15,9 +12,10 @@ public class main {
     private static double g = 9.82;                     //Gravity
     private static double hf = 0;                       //Horizontal Force
     private static double hmax;                         //Max bounce Height
-    private static double hstop = 0.01;                 //Minimum required max height
-    private static double k = 0.9;                      //Bounce coefficient
+    private static double hstop = 0.5;                  //Minimum required max height
+    private static double k = 0.6;                      //Bounce coefficient
     private static double v = 0;                        //Velocity
+    private static double vt = 0;                       //Velocity time
 
     public static void main(String[] arg){
 
@@ -25,26 +23,46 @@ public class main {
         String inputmessage = "";
         valueinputter(inputtext, inputmessage);
         if (trueorfalse){
-            h(t);
+            System.out.println("Starting calculations");
+            h();
         }
         else {
             valueinputter(inputtext, inputmessage);
         }
     }
 
-    private static void h(double t) {
+    private static void h() {
         while (hmax > hstop) {
-            if (!contact) {
-                System.out.println(h + " " + t + " " + v);
+                System.out.println(h);
                 if (fallingdown) {
-                    
+                    v = -(9.82*(vt*vt));
+                    vt += 0.01;
+                    if (h <= 0) {
+                        fallingdown = false;
+                        t += ct;
+                        v = -v * k;
+                        vt = 0;
+                        //System.out.println("made v negative");
+                        if (t > 20) {
+                            hmax = 0;
+                            System.out.println("Took too long!");
+                        }
+                    }
                 }
-                h += v/100;
+                else if (!contact){
+                    v -= 9.82*(vt*vt);
+                    vt += 0.01;
+                    if (v <= 0) {
+                        fallingdown = true;
+                        hmax = h;
+                        //System.out.println("Reached apex and set hmax to h = " + hmax);
+                    }
+                }
+                h += v;
                 t += dt;
-
             }
         }
-    }
+
     static boolean valueinputter(String inputtext, String inputmessage) {
         inputmessage = "Please input desired dropheight in meters: ";
         inputtext = JOptionPane.showInputDialog("Ball weight: " + g + "\n" + "Ball dropheight: " + h + "\n" + "Plane angle: " + hf + "\n" + "\n" + inputmessage);
